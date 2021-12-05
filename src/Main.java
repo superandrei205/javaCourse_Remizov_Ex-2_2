9d6f22856ee5563213b6472ab5858eded6c065e9
aremizov <aremizov@aori.ru>
2021-12-05 18:21:42 +0300

    ex2_2

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        String city = "Moscow";
+        int population = 1000;
+        float withOutWorkPercent = 0.2f;
+        System.out.println("Город: " + city + "; Население: " + population + "; Процент безработных: " + withOutWorkPercent);
+
+        char section = 'A';
+        int seat = 1;
+        long price = 9000000;
+        System.out.println("Секция: " + section + "; Место: " + seat + "; Стоимость билета: " + price);
+
+
+        String name = "Andrei";
+        short age = 29;
+        short height = 205;
+        System.out.print("Name: " + name + "; Age: " + age + "; Height: " + height);
+    }
+}
